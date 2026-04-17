5b735c59ac13017d272bc299d351b1e8f03509a1
Ian <placeholder@placeholder.com>
2026-04-17 17:44:41 +0200

    fix(ci): add pytest-cov and convert jest config to js

---
+++ web/jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('jest').Config} */
+const config = {
+  // el mateix contingut que jest.config.ts però sense types
+  // canvia: export default { ... }
+  // per:    module.exports = { ... }
+}
+
+module.exports = config
